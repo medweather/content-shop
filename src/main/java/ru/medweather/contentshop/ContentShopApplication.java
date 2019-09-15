726f196a328f9b84fdff15780ff1712b153d16e3
Alexandr Medvedev <medweather@mail.ru>
2019-09-15 13:19:49 +0300

    initial project

---
+++ src/main/java/ru/medweather/contentshop/ContentShopApplication.java
@@ -0,0 +1,13 @@
+package ru.medweather.contentshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/medweather/contentshop/ContentShopApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.medweather.contentshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContentShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
